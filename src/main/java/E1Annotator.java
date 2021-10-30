baad5fb418037f49f569c9ec1ac7294b6e2622aa
ken437 <ken437@vt.edu>
2021-10-29 21:30:28 -0400

    got it to actually highlight real E1 issues

--- src/main/java/E1Annotator.java
+++ src/main/java/E1Annotator.java
@@ -1,6 +1,10 @@
 import com.intellij.psi.PsiElement;
-import com.jetbrains.python.psi.PyStringLiteralExpression;
+import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.impl.references.PyOperatorReference;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * Annotates the E1 issue with a warning highlight and descriptive message
@@ -8,20 +12,66 @@ import org.jetbrains.annotations.NotNull;
 public class E1Annotator extends PythonifyAnnotator {
     @Override
     public PythonifyQuickFix getQuickFix(PsiElement element) {
-        return new E1QuickFix(element);
+        return null;
     }
 
     @Override
     public @NotNull String getMessage() {
-        return "E1: This string is not Bobby enough!";
+        return "E1: Access from end of array without negative index";
     }
 
     @Override
     public boolean shouldAnnotate(PsiElement element) {
-        if (!(element instanceof PyStringLiteralExpression)) {
+        /*
+        if (!(Objects.equals(element.getText(), "b[len(b) - 2]")))
+        {
             return false;
         }
-        PyStringLiteralExpression stringElem = (PyStringLiteralExpression) element;
-        return !(stringElem.getStringValue().equals("Hello, my name is Bob"));
+        */
+        if (!(element instanceof PySubscriptionExpression))
+        {
+            return false;
+        }
+        PySubscriptionExpression subElem = (PySubscriptionExpression) element;
+        PsiElement[] subChildren = subElem.getChildren();
+        if (subChildren.length != 2)
+        {
+            return false;
+        }
+        if (!(subChildren[0] instanceof PyReferenceExpression && subChildren[1] instanceof PyBinaryExpression))
+        {
+            return false;
+        }
+        PyReferenceExpression refBeforeSub = (PyReferenceExpression) subChildren[0];
+        PyBinaryExpression binAfterSub = (PyBinaryExpression) subChildren[1];
+        PyExpression leftPart = binAfterSub.getLeftExpression();
+        PyExpression rightPart = binAfterSub.getRightExpression();
+        PyElementType opType = binAfterSub.getOperator();
+        if (!(leftPart instanceof PyCallExpression && rightPart instanceof PyNumericLiteralExpression))
+        {
+            return false;
+        }
+        PyCallExpression leftCall = (PyCallExpression) leftPart;
+        PyNumericLiteralExpression litRight = (PyNumericLiteralExpression) rightPart;
+        Long rightNum = litRight.getLongValue();
+        if (opType == null || rightNum == null || rightNum <= 0)
+        {
+            return false;
+        }
+        if (!(opType.toString().equals("Py:MINUS")))
+        {
+            return false;
+        }
+        if (!(leftCall.isCalleeText("len")))
+        {
+            return false;
+        }
+        PyExpression[] leftCallArgs = leftCall.getArguments();
+        if (leftCallArgs.length != 1 || !(leftCallArgs[0] instanceof PyReferenceExpression))
+        {
+            return false;
+        };
+        PyReferenceExpression leftCallArg = (PyReferenceExpression) leftCallArgs[0];
+        return (Objects.equals(refBeforeSub.getName(), leftCallArg.getName()));
     }
 }
--- src/main/python/Example10.py
+++ src/main/python/Example10.py
@@ -4,3 +4,5 @@ from math import *
 def hi():
     a = "Hello, my name is Bob"
     print(sqrt(2))
+    b = [1, 2, 3]
+    print(b[len(b) - 1])
