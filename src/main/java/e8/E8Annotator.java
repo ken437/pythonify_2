849fc0db2d0447f09a5cddb48b2f045d0e9f0338
ken437 <ken437@vt.edu>
2022-01-25 01:05:39 -0500

    Got E8 to work

--- src/main/java/e8/E8Annotator.java
+++ src/main/java/e8/E8Annotator.java
@@ -1,12 +1,15 @@
-package e2;
+package e8;
 
 import com.intellij.psi.PsiElement;
+import com.jetbrains.python.psi.PyCallExpression;
+import com.jetbrains.python.psi.PyExpression;
 import com.jetbrains.python.psi.PyFromImportStatement;
+import com.jetbrains.python.psi.PyWithItem;
 import general.PythonifyAnnotator;
 import general.PythonifyQuickFix;
 import org.jetbrains.annotations.NotNull;
 
-public class E2Annotator extends PythonifyAnnotator {
+public class E8Annotator extends PythonifyAnnotator {
     @Override
     public PythonifyQuickFix getQuickFix(PsiElement element) {
         return null;
@@ -14,16 +17,31 @@ public class E2Annotator extends PythonifyAnnotator {
 
     @Override
     public @NotNull String getMessage() {
-        return "E2: Star imports are not recommended";
+        return "E8: Use a with statement when opening a file using open()";
     }
 
     @Override
     public boolean shouldAnnotate(PsiElement element) {
-        if (!(element instanceof PyFromImportStatement))
+        if (!(element instanceof PyCallExpression))
         {
             return false;
         }
-        PyFromImportStatement fromImportElem = (PyFromImportStatement) element;
-        return (fromImportElem.getStarImportElement() != null);
+        PyCallExpression callExpr = (PyCallExpression) element;
+        PyExpression callee = callExpr.getCallee();
+        if (callee == null)
+        {
+            return false;
+        }
+        if (callee.getChildren().length != 0) // handles cases like self.open()
+        {
+            return false;
+        }
+        if (!(callExpr.isCalleeText("open")))
+        {
+            return false;
+        }
+
+        PsiElement parent = element.getParent();
+        return !(parent instanceof PyWithItem);
     }
 }
