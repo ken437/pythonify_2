badf3c79369c12c438b02d4f2762b7aa5e89b2cb
ken437 <ken437@vt.edu>
2022-01-25 00:36:15 -0500

    added extra E8 test cases

---
+++ src/main/java/e8/E8Annotator.java
@@ -0,0 +1,29 @@
+package e2;
+
+import com.intellij.psi.PsiElement;
+import com.jetbrains.python.psi.PyFromImportStatement;
+import general.PythonifyAnnotator;
+import general.PythonifyQuickFix;
+import org.jetbrains.annotations.NotNull;
+
+public class E2Annotator extends PythonifyAnnotator {
+    @Override
+    public PythonifyQuickFix getQuickFix(PsiElement element) {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getMessage() {
+        return "E2: Star imports are not recommended";
+    }
+
+    @Override
+    public boolean shouldAnnotate(PsiElement element) {
+        if (!(element instanceof PyFromImportStatement))
+        {
+            return false;
+        }
+        PyFromImportStatement fromImportElem = (PyFromImportStatement) element;
+        return (fromImportElem.getStarImportElement() != null);
+    }
+}
--- src/test/testData/e8/e8_test_highlight.py
+++ src/test/testData/e8/e8_test_highlight.py
@@ -38,3 +38,16 @@ def wedontclosefiles():
     with open("I'm a file") as f:
         # Extraneous comment
         f.write("Content!")
+
+class NotFileUser:
+    def open(self, fake_filename, fake_filemode):
+        print("I'm not opening a file!")
+        return 1
+
+    def dont_highlight_here(self):
+        f = self.open("I'm not a file!", "I'm not a filemode!")
+        print(f)
+
+from PIL import Image
+def pillow():
+    im = Image.open("Image file", "mode")
